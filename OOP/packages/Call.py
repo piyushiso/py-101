a166edb34ae1e3a95db765f8c788dd4e8cd6e170
Piyush L. Shrestha <piyushshrestha20@gmail.com>
2026-03-23 08:10:12 +0545

    Updating POOP

---
+++ OOP/packages/Call.py
@@ -0,0 +1,5 @@
+class Call:
+    def __init__(self, name):
+        self.name = name
+    def call(self):
+        print("Calling {self.name}")
---
+++ OOP/test.py
@@ -0,0 +1,4 @@
+from packages import Call
+
+call = Call()
+call.call()
